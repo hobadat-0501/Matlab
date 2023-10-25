456657c488146c6c29a191f4b19d99c96a13714d
hobadat-0501 <hobadatyv@gmail.com>
2023-10-26 00:24:32 +0700

    update

--- bt/tieptuyen.m
+++ bt/tieptuyen.m
@@ -1,14 +1,13 @@
-%bài 4.2
-function [x1, ep, n] = tieptuyen(f, x0, err, nmax)
+function [x1, ep, n] = tieptuyen(f, x0, ss, nmax)
     n = 0;
-    ep = err;
-    while (ep >= err) && (n < nmax)
-        df = diff(f);
-        x1 = x0 - subs(f, x0) / subs(df, x0);
-        ep = abs((x1 - x0) / x1); % Sửa ep=abs((x1-x0)/x0) thành ep=abs((x1-x0)/x1)
+    ep = ss;
+    while (ep >= ss) && (n < nmax)
+        x1 = x0 - subs(f, x0) / subs(diff(f), x0);
+        ep = abs((x1 - x0) / x0);
         n = n + 1;
         x0 = x1;
     end
+
     x1 = double(x1);
-    ep = double(ep);
+    ep = double(ep)
 end
